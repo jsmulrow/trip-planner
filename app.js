e52feda8b2a41ddb82137135fc030cb935da0f03
GregAMeyer <gregmeyer888@gmail.com>
2015-07-01 16:31:46 -0400

    apikey

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ var swig = require('swig');
 
 
 var routes = require('./routes/index');
-var users = require('./routes/users');
+
 
 var app = express();
 
@@ -38,7 +38,7 @@ app.use(express.static(path.join(__dirname, 'bower_components')));
 
 
 app.use('/', routes);
-app.use('/users', users);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
